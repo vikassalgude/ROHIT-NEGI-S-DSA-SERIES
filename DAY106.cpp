3a50d1a7fff41275f1b6063304c46730b97d4364
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-02-19 16:59:12 +0530

    Update DAY106.cpp

--- DAY106.cpp
+++ DAY106.cpp
@@ -165,6 +165,49 @@ class Solution {
     }
 };
 
+//BOTTOM VIEW OF BT
+class Solution {
+  public:
+   void find(Node* root,int pos,int&l,int&r){
+        if(!root)
+          return;
+        l=min(pos,l);
+        r=max(pos,r);
+        find(root->left,pos-1,l,r);
+        find(root->right,pos+1,l,r);
+    }
+  
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        int l=0,r=0;
+        find(root,0,l,r);
+        vector<int>ans(r-l+1);
+        vector<int>filled(r-l+1,0);
+        queue<Node*>q;
+        queue<int>index;
+        q.push(root);
+        index.push(-1*l);
+        while(!q.empty()){
+            Node *t=q.front();
+            q.pop();
+            int pos=index.front();
+            index.pop();
+            
+            filled[pos]=1;
+            ans[pos]=t->data;
+         
+            if(t->left){
+                q.push(t->left);
+                index.push(pos-1);
+            }
+            if(t->right){
+                q.push(t->right);
+                index.push(pos+1);
+            }
+        }
+        return ans;
+    }
+};
 
 
     
