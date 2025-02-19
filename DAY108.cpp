8dbaad786bde21d26d1cabc6568649865c011bfc
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-02-20 01:13:33 +0530

    Create DAY108.cpp

---
+++ DAY108.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int find(vector<int>in,int target,int start,int end){
+        for(int i=start;i<=end;i++){
+            if(in[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+    Node *tree(vector<int>in,vector<int>pre,int instart,int inend,int index){
+        if(instart>inend){
+            return NULL;
+        }
+        Node* root=new Node(pre[index]);
+        int pos=find(in,pre[index],instart,inend);
+        root->left=tree(in,pre,instart,pos-1,index+1);
+        root->right=tree(in,pre,pos+1,inend,index+1+(pos-instart));
+        return root;
+        
+    }
+    // Function to build the tree from given inorder and preorder traversals
+    Node *buildTree(vector<int> &in, vector<int> &pre) {
+        // code here
+        int n=in.size();
+        return tree(in,pre,0,n-1,0);
+    }
+};
