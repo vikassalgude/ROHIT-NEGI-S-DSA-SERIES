c43afb697f1bf4d0b111f5b37bfcfe8630187e16
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-02-23 11:04:13 +0530

    Update DAY108.cpp

--- DAY108.cpp
+++ DAY108.cpp
@@ -26,3 +26,34 @@ class Solution {
         return tree(in,pre,0,n-1,0);
     }
 };
+
+class Solution {
+  public:
+        int find(vector<int>in,int target,int start,int end){
+        for(int i=start;i<=end;i++){
+            if(in[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+       Node *tree(vector<int>in,vector<int>post,int instart,int inend,int index){
+        if(instart>inend){
+            return NULL;
+        }
+        Node* root=new Node(post[index]);
+        int pos=find(in,post[index],instart,inend);
+        root->right=tree(in,post,pos+1,inend,index-1);
+        root->left=tree(in,post,instart,pos-1,index-(inend-pos)-1);
+        
+        return root;
+        
+    }
+
+    // Function to return a tree created from postorder and inoreder traversals.
+    Node* buildTree(vector<int> in, vector<int> post) {
+        // code here
+        int n=in.size();
+        return tree(in,post,0,n-1,n-1);
+    }
+};
