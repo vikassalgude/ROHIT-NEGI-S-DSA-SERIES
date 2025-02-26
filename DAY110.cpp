f7b1fc25606ef37f148f7a4ac231b9f2ced289fe
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-02-26 12:32:41 +0530

    Update DAY110.cpp

--- DAY110.cpp
+++ DAY110.cpp
@@ -96,3 +96,28 @@ class Solution{
         return ans;
     }
 };
+// FLATTEN BINARY TREE TO LINKED LIST
+class Solution
+{
+    public:
+    void flatten(Node *root)
+    {
+        //code here
+        while(root){
+            if(!root->left){
+                root=root->right;
+            }
+            else{
+                Node *curr=root->left;
+                while(curr->right){
+                    curr=curr->right;
+                }
+                curr->right=root->right;
+                root->right=root->left;
+                root->left=NULL;
+                root=root->right;
+            }
+        }
+    }
+};
+
